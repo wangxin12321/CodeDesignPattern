4c120b3767777e91c231babc3d2f0015d4bb6ffe
Tony <tony@gmail.com>
2019-10-26 16:58:22 +0800

    图片加载1.0,在MainActivity中实现图片加载

--- app/src/main/java/wx/swpu/edu/codedesignpattern/MainActivity.java
+++ app/src/main/java/wx/swpu/edu/codedesignpattern/MainActivity.java
@@ -3,12 +3,19 @@ package wx.swpu.edu.codedesignpattern;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private ImageLoader imageLoader;
+    private String url="https://p0.ssl.qhimgs1.com/bdr/800__/t0187f7f6d0515b56ae.jpg";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView=findViewById(R.id.iv);
+        imageLoader.displayImage(url,imageView);
     }
 }
